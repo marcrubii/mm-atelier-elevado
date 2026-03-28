8975da3a8a54181cadf47ee1e22c4363d08a4c04
marcrubii <marc.rubi@estudiantat.upc.edu>
2026-03-28 17:31:31 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/mm-atelier-elevado/",
   server: {
     host: "::",
     port: 8080,
