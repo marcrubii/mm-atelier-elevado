b656f75c322c35f619058724b4070a0d54ed8515
marcrubii <marc.rubi@estudiantat.upc.edu>
2026-03-28 23:57:22 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const routerBasename = window.location.pathname.startsWith("/mm-atelier-elevado")
-  ? "/mm-atelier-elevado"
-  : "/";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={routerBasename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
